8da4e1cffe3cda026b520aef33356ead70cca9ac
Dmitry Vorotilin <d.vorotilin@gmail.com>
2012-12-25 15:18:38 +0400

    Removed useless print

--- lib/active_record/connection_adapters/vertica_adapter.rb
+++ lib/active_record/connection_adapters/vertica_adapter.rb
@@ -999,7 +999,6 @@ module ActiveRecord
         UNIQUE_VIOLATION      = "23505"
 
         def translate_exception(exception, message)
-          p exception
           case exception.result.error_field(PGresult::PG_DIAG_SQLSTATE)
           when UNIQUE_VIOLATION
             RecordNotUnique.new(message, exception)
